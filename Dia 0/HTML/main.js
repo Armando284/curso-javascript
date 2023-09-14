97d4b3c4a24088c45cffe5fb599ceab86b11eeae
Armando Peña Tamayo <arman2p284@gmail.com>
2023-09-14 15:31:53 -0400

    Fix: Eliminating course folder

--- Dia 0/HTML/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * Este es un sencillo programa en JavaScript que 
- * tomara un elemento de el HTML por su id 
- * e igualara su contenido de texto a el mensaje 'Hello, World!'
- */
-
-const textElement = document.getElementById('my-text');
-
-textElement.innerText = 'Hello, World!';
